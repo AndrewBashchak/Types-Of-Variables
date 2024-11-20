8d74cca227e74c7796cbe67eb198e436f2fdafef
AndrewBashchak <g.a.p.bloger@gmail.com>
2024-11-20 21:18:59 +0200

    Types of variables and their simple use

---
+++ TypesOfVariables.js
@@ -0,0 +1,9 @@
+const device = "Personal computer (PC)";
+const processorName = "Intel Core i9-14900K 3.2GHz/36MB (BX8071514900K) s1700 BOX";
+const releaseDate = "2025 year";
+
+let price = 900;
+let availability = "Yes";
+let buyer = "Scott Cawthon";
+
+console.log(`Device: ${device}; Processor name: ${processorName}; Release Date: ${releaseDate}; The price of the device: ${price} dollars; Availability: ${availability}; Buyer: ${buyer}`)
