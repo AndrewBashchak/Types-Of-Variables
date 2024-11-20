91353980083cdc4131a6d1db2b49b747a4de052b
Bloger_G.A.P. <g.a.p.bloger@gmail.com>
2024-11-20 21:20:54 +0200

    Update TypesOfVariables.js

--- TypesOfVariables.js
+++ TypesOfVariables.js
@@ -1,4 +1,4 @@
-const device = "Personal computer (PC)";
+const device = "Personal computer (PC)";.
 const processorName = "Intel Core i9-14900K 3.2GHz/36MB (BX8071514900K) s1700 BOX";
 const releaseDate = "2025 year";
 
